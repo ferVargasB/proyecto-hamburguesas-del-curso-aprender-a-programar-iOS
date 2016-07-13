4ee5dfbe56059e10cd5fdae88bbf6882f76880ec
ferVargasB <f.vargas.bravo@icloud.com>
2016-07-12 23:30:16 -0500

    Se añadieron las clases Países y Hamburguesas con sus respectivos atributos y métodos

--- Hamburguesas/Datos.swift
+++ Hamburguesas/Datos.swift
@@ -8,3 +8,25 @@
 
 import Foundation
 import UIKit
+
+class ColeccionPaises {
+    var paises = ["Argentina", "Barbados", "Corea del Sur", "Israel",
+        "Japón", "Taiwán", "Alemania", "Andorra", "Austria", "Bélgica",
+        "Chipre", "Dinamarca", "Eslovaquia", "Eslovenia", "España",
+        "Finlandia", "Francia", "Grecia", "Irlanda", "Islandia", "Italia", "Liechtenstein", "Luxemburgo", "Malta", "Mónaco", "Noruega", "Países Bajos", "Portugal", "República Checa", "Reino Unido", "San Marino", "Suecia", "Suiza", "Oceanía", "Australia", "Nueva Zelanda", "Canadá", "Estados Unidos"]
+    
+    func obtenerPais() -> String {
+        let posicion = Int(arc4random()) % paises.count
+        return paises[posicion]
+    }
+}
+
+class ColeccionHamburguesas {
+    var hamburguesas = ["Big Mac", "Cuarto de libra", "Big Tasty", "McRib",
+                        "Hamburguesa doble con queso", "Hamburguesa con queso", "McNÍFICA", "Angus CBO", "Angus Premium Tocino ", "Whopper BQR", "Whopper con Queso", "Whopper Angry", "Big King", "X-Treme", "Mega XT", "The Big Carl", "Guacamole Bacon", "Western Bacon", "Jalapeño Burguer", "Single Teriyaki"]
+    
+    func obtenerPais() -> String {
+        let posicion = Int(arc4random()) % hamburguesas.count
+        return hamburguesas[posicion]
+    }
+}
